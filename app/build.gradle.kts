d0eb6e6ba78659669db2e356a7a650443c258d65
Khaled <spkhalad@gmail.com>
2024-04-03 12:25:34 +0600

    [skip ci] Bump up to v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "dev.khaled.leanstream"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         vectorDrawables {
             useSupportLibrary = true
         }
